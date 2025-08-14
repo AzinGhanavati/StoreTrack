02295320b1364b3ae174d7bb5605c640eea9a1f3
Elmira <100311497+elmirapanahi@users.noreply.github.com>
2025-08-14 21:31:39 +0330

    Update orderController.js

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -143,22 +143,3 @@ exports.getSalesReport = async (req, res) => {
         res.status(400).json({ success: false, error: err.message });
     }
 };
-// exports.getSalesReport = async (req, res) => {
-//     try {
-//         const sales = await Order.aggregate([
-//             { $match: { status: 'ارسال شده' } },
-//             {
-//                 $group: {
-//                     _id: { $dateToString: { format: "%Y-%m-%d", date: "$orderDate" } },
-//                     totalSales: { $sum: "$totalPrice" },
-//                     count: { $sum: 1 }
-//                 }
-//             },
-//             { $sort: { _id: 1 } }
-//         ]);
-//         res.status(200).json({ success: true, data: sales });
-//     } catch (err) {
-//         res.status(400).json({ success: false, error: err.message });
-//     }
-// };
-
